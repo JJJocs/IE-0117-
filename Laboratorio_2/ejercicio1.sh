10f8bdf6967ca9896f508af6a9559cdd200125b5
Jocs <jocser.calderon@ucr.ac.cr>
2024-04-11 12:17:23 -0600

    Desarrollo completo de lógica, es necesario manejar errores y comentar código

--- Laboratorio_2/ejercicio1.sh
+++ Laboratorio_2/ejercicio1.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 ID=$1
 info_proceso=$(ps aux | awk '$2 == "'$ID'" {print $0}')
+nombre_proceso=$(ps aux | awk '$2 == '"$ID"' {print $12}')
+parent_processID=$(ps -f $ID | awk '$2 == '"$ID"' {print $3}')
+usuario_propietario=$(ps aux | awk '$2 == '"$ID"' {print $1}')
+porcentaje_uso_cpu=$(ps aux | awk '$2 == '"$ID"' {print $3}')
+consumo_memoria=$(ps aux | awk '$2 == '"$ID"' {print $4}')
+estado=$(ps aux | awk '$2 == '"$ID"' {print $8}')
+path=$(lsof | awk '$2 == "'$ID'" {print $12}')
 
 
-echo "La información del proceso asociado al ID: $ID es la siguiente:"
-echo "$info_proceso"
- 
- 
-path=$(pwd $ID)
-echo "El path del proceso con PID $ID es: $path"
 
+mostrar_informacion_proceso() {
+    echo "La información del proceso asociado al ID: $ID es la siguiente:"
+    echo "Nombre del proceso: $nombre_proceso"
+    echo "ID del proceso: $ID"
+    echo "Parent process ID: $parent_processID"
+    echo "Usuario propietario: $usuario_propietario"
+    echo "Porcentaje de uso de CPU : $porcentaje_uso_cpu"
+    echo "Consumo de memoria: $consumo_memoria"
+    echo "Estado (status): $estado"
+    echo "Path del ejecutable: $path"
+}
 
+mostrar_informacion_proceso
