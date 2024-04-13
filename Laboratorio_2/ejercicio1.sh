2924b334b8e5308222c766407d776d9ea88ae44d
Jocs <jocser.calderon@ucr.ac.cr>
2024-04-13 00:07:16 -0600

    baba

--- Laboratorio_2/ejercicio1.sh
+++ Laboratorio_2/ejercicio1.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+
+# Verifica si se proporciona un ID de proceso como argumento
+if [ -z "$1" ]; then
+    echo "Por favor, proporcione el ID del proceso como argumento."
+    exit 1
+fi
+
 ID=$1
-info_proceso=$(ps aux | awk '$2 == "'$ID'" {print $0}')
-nombre_proceso=$(ps aux | awk '$2 == '"$ID"' {print $12}')
+
+# Verifica si el proceso asociado al ID existe
+if ! ps -p $ID > /dev/null; then
+    echo "El proceso con el ID $ID no se encuentra."
+    exit 1
+fi
+
+# Obtén información del proceso asociado al ID
+nombre_proceso=$(ps -p $ID -o comm=)
 parent_processID=$(ps -f $ID | awk '$2 == '"$ID"' {print $3}')
-usuario_propietario=$(ps aux | awk '$2 == '"$ID"' {print $1}')
-porcentaje_uso_cpu=$(ps aux | awk '$2 == '"$ID"' {print $3}')
-consumo_memoria=$(ps aux | awk '$2 == '"$ID"' {print $4}')
-estado=$(ps aux | awk '$2 == '"$ID"' {print $8}')
-path=$(lsof | awk '$2 == "'$ID'" {print $12}')
+usuario_propietario=$(ps -o user= -p $ID)
+porcentaje_uso_cpu=$(ps -p $ID -o %cpu=)
+consumo_memoria=$(ps -p $ID -o %mem=)
+estado=$(ps -p $ID -o stat=)
+path=$(sudo readlink /proc/$ID/exe)
 
-
-
+# Función para mostrar la información del proceso
 mostrar_informacion_proceso() {
     echo "La información del proceso asociado al ID: $ID es la siguiente:"
     echo "Nombre del proceso: $nombre_proceso"
@@ -20,7 +33,6 @@ mostrar_informacion_proceso() {
     echo "Porcentaje de uso de CPU : $porcentaje_uso_cpu"
     echo "Consumo de memoria: $consumo_memoria"
     echo "Estado (status): $estado"
-    echo "Path del ejecutable: $path"
 }
 
 mostrar_informacion_proceso
